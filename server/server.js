f0cf29192ac10d32433594e285b20e32b653c605
Nicole Kowtko <akowtko@hmc.edu>
2019-01-13 14:01:08 -0800

    add GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
+var {ObjectID} = require('mongodb');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
@@ -10,7 +11,7 @@ app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
     var todo = new Todo({
-       text: req.body.text,
+        text: req.body.text,
     });
     todo.save().then((doc) => {
         res.send(doc._doc);
@@ -20,9 +21,20 @@ app.post('/todos', (req, res) => {
 });
 
 app.get('/todos', (req, res) => {
-   Todo.find().then((todos) => {
-      res.send({todos});
-   }, (e) => res.status(400).send(e));
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (e) => res.status(400).send(e));
+});
+
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    Todo.findById(id).then((todo) => {
+        if (!todo) return res.status(404).send();
+        res.send({todo});
+    }).catch(e => res.status(400).send());
 });
 
 app.listen(3000, () => {
