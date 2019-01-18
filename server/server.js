9f1c50012511bf3ce854fd773efd529ad45130ea
Nicole Kowtko <akowtko@hmc.edu>
2019-01-18 15:33:04 -0800

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const _ = require('lodash');
 const {ObjectID} = require('mongodb');
+const bcrypt = require('bcryptjs');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -89,6 +90,16 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+    const body =  _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+             res.header('x-auth', token).send(user);
+        });
+    }).catch(e => res.status(400).send());
 });
 
 app.listen(port, () => {
